5e1dcf9a611122064296cbc2b2e9e13eedc2cb6e
Sumit Rathi <sumitrathi95@gmail.com>
2020-07-26 02:00:00 +0530

    Commit

---
+++ routes/customers.js
@@ -0,0 +1,297 @@
+var express = require('express')
+var app = express()
+var path = require('path');
+
+// SHOW LIST OF USERS
+app.get('/list', function(req, res, next) {
+	req.getConnection(function(error, conn) {
+		conn.query('SELECT CustomerID,CompanyName,ContactName,ContactTitle,Address,City,Region,PostalCode,Country,Phone,Fax FROM Customers ORDER BY CustomerID DESC',function(err, rows, fields) {
+			//if(err) throw err
+			if (err) {
+				req.flash('error', err)
+				res.render('customers/list', {
+					title: 'Customers List',
+					data: ''
+				})
+			} else {
+				// render to views/user/list.ejs template file
+				res.render('customers/list', {
+					title: 'Customers List',
+					data: rows
+				})
+			}
+		})
+	})
+})
+
+app.get('/', function(req, res, next) {
+				res.redirect('list')
+			}
+		)
+
+// SHOW ADD USER FORM
+app.get('/add', function(req, res, next){
+	// render to views/user/add.ejs
+			res.render('customers/add', {
+				title: 'Add New Customer',
+				CustomerID:'',
+				CompanyName:'',
+				ContactName:'',
+				ContactTitle:'',
+				Address:'',
+				City:'',
+				Region:'',
+				PostalCode:'',
+				Country:'',
+				Phone:'',
+				Fax:''
+				})
+			})
+
+// ADD NEW USER POST ACTION
+app.post('/add', function(req, res, next){
+	req.assert('CustomerID', 'CustomerID is required').notEmpty()           //Validate name
+	req.assert('ContactName', 'ContactName is required').notEmpty()             //Validate age
+    var errors = req.validationErrors()
+
+    if( !errors ) {   //No errors were found.  Passed Validation!
+
+		var customers = {
+			CustomerID: req.sanitize('CustomerID').escape().trim(),
+			CompanyName: req.sanitize('CompanyName').escape().trim(),
+			ContactName: req.sanitize('ContactName').escape().trim(),
+			ContactTitle: req.sanitize('ContactTitle').escape().trim(),
+			Address: req.sanitize('Address').escape().trim(),
+			City: req.sanitize('City').escape().trim(),
+			Region: req.sanitize('Region').escape().trim(),
+			PostalCode: req.sanitize('PostalCode').escape().trim(),
+			Country: req.sanitize('Country').escape().trim(),
+			Phone: req.sanitize('Phone').escape().trim(),
+			Fax: req.sanitize('Fax').escape().trim()
+		}
+
+		req.getConnection(function(error, conn) {
+			conn.query("INSERT INTO Customers values ('"+req.body.CustomerID+"','"+req.body.CompanyName+"','"+req.body.ContactName+"','"+req.body.ContactTitle+"','"+req.body.Address+"','"+req.body.City+"','"+req.body.Region+"','"+req.body.PostalCode+"','"+req.body.Country+"','"+req.body.Phone+"','"+req.body.Fax+"')", function(err, result) {
+				//if(err) throw err
+				if (err) {
+					req.flash('error', err)
+
+					// render to views/user/add.ejs
+					res.render('customers/add', {
+						title: 'Add New Customer',
+						CustomerID:customers.CustomerID,
+						CompanyName:customers.CompanyName,
+						ContactName:customers.ContactName,
+						ContactTitle:customers.ContactTitle,
+						Address:customers.Address,
+						City:customers.City,
+						Region:customers.Region,
+						PostalCode:customers.PostalCode,
+						Country:customers.Country,
+						Phone:customers.Phone,
+						Fax:customers.Fax
+					})
+				} else {
+					req.flash('success', 'Data added successfully!')
+
+					// render to views/user/add.ejs
+					res.render('customers/add', {
+						title: 'Add New Customer',
+						CustomerID:'',
+						CompanyName:'',
+						ContactName:'',
+						ContactTitle:'',
+						Address:'',
+						City:'',
+						Region:'',
+						PostalCode:'',
+						Country:'',
+						Phone:'',
+						Fax:''
+					})
+				}
+			})
+		})
+	}
+	else {   //Display errors to user
+		var error_msg = ''
+		errors.forEach(function(error) {
+			error_msg += error.msg + '<br>'
+		})
+		req.flash('error', error_msg)
+
+		/**
+		 * Using req.body.name
+		 * because req.param('name') is deprecated
+		 */
+        res.render('customers/add', {
+            title: 'Add New Customer',
+            CustomerID: req.body.CustomerID,
+            CompanyName: req.body.CompanyName,
+            ContactName: req.body.ContactName,
+						ContactTitle: req.body.ContactTitle,
+            Address: req.body.Address,
+            City: req.body.City,
+						Region: req.body.Region,
+            PostalCode: req.body.PostalCode,
+            Country: req.body.Country,
+						Phone: req.body.Phone,
+						Fax: req.body.Fax
+        })
+    }
+})
+
+// SHOW EDIT USER FORM
+app.get('/edit/(:CustomerID)', function(req, res, next){
+	req.getConnection(function(error, conn) {
+		conn.query('SELECT * FROM Customers WHERE CustomerID = ?', [req.params.CustomerID], function(err, rows, fields) {
+			if(err) throw err
+
+			// if user not found
+			if (rows.length <= 0) {
+				req.flash('error', 'Customer not found with CustomerID = ' + req.params.CustomerID)
+				res.redirect('/customers')
+			}
+			else { // if user found
+				// render to views/user/edit.ejs template file
+				res.render('customers/edit', {
+					title: 'Edit Customer',
+					//data: rows[0],
+					CustomerID: rows[0].CustomerID,
+					CompanyName: rows[0].CompanyName,
+					ContactName: rows[0].ContactName,
+					ContactTitle: rows[0].ContactTitle,
+					Address: rows[0].Address,
+					City: rows[0].City,
+					Region: rows[0].Region,
+					PostalCode: rows[0].PostalCode,
+					Country: rows[0].Country,
+					Phone: rows[0].Phone,
+					Fax: rows[0].Fax
+				})
+			}
+		})
+	})
+})
+
+// EDIT USER POST ACTION
+app.put('/edit/(:CustomerID)', function(req, res, next) {
+	req.assert('CustomerID', 'CustomerID is required').notEmpty()
+	req.assert('ContactName', 'ContactName is required').notEmpty()
+
+    var errors = req.validationErrors()
+
+    if( !errors ) {   //No errors were found.  Passed Validation!
+
+		/********************************************
+		 * Express-validator module
+
+		req.body.comment = 'a <span>comment</span>';
+		req.body.username = '   a user    ';
+
+		req.sanitize('comment').escape(); // returns 'a &lt;span&gt;comment&lt;/span&gt;'
+		req.sanitize('username').trim(); // returns 'a user'
+		********************************************/
+		var customers = {
+			CustomerID: req.sanitize('CustomerID').escape().trim(),
+			CompanyName: req.sanitize('CompanyName').escape().trim(),
+			ContactName: req.sanitize('ContactName').escape().trim(),
+			ContactTitle: req.sanitize('ContactTitle').escape().trim(),
+			Address: req.sanitize('Address').escape().trim(),
+			City: req.sanitize('City').escape().trim(),
+			Region: req.sanitize('Region').escape().trim(),
+			PostalCode: req.sanitize('PostalCode').escape().trim(),
+			Country: req.sanitize('Country').escape().trim(),
+			Phone: req.sanitize('Phone').escape().trim(),
+			Fax: req.sanitize('Fax').escape().trim()
+		}
+
+		req.getConnection(function(error, conn) {
+			conn.query("UPDATE Customers SET CustomerID='"+req.body.CustomerID+"',CompanyName='"+req.body.CompanyName+"',ContactName='"+req.body.ContactName+"',ContactTitle='"+req.body.ContactTitle+"',Address='"+req.body.Address+"',City='"+req.body.City+"',Region='"+req.body.Region+"',PostalCode='"+req.body.PostalCode+"',Country='"+req.body.Country+"',Phone='"+req.body.Phone+"',Fax='"+req.body.Fax+"' WHERE CustomerID =  '"+ req.params.CustomerID+"'", function(err, result) {
+				//if(err) throw err
+				if (err) {
+					req.flash('error', err)
+
+					// render to views/user/add.ejs
+					res.render('customers/edit', {
+						title: 'Edit Customer',
+						CustomerID: req.body.CustomerID,
+            CompanyName: req.body.CompanyName,
+            ContactName: req.body.ContactName,
+						ContactTitle: req.body.ContactTitle,
+            Address: req.body.Address,
+            City: req.body.City,
+						Region: req.body.Region,
+            PostalCode: req.body.PostalCode,
+            Country: req.body.Country,
+						Phone: req.body.Phone,
+						Fax: req.body.Fax
+					})
+				} else {
+					req.flash('success', 'Data updated successfully!')
+
+					// render to views/user/add.ejs
+					res.render('customers/edit', {
+						title: 'Edit Customer',
+						CustomerID: req.body.CustomerID,
+            CompanyName: req.body.CompanyName,
+            ContactName: req.body.ContactName,
+						ContactTitle: req.body.ContactTitle,
+            Address: req.body.Address,
+            City: req.body.City,
+						Region: req.body.Region,
+            PostalCode: req.body.PostalCode,
+            Country: req.body.Country,
+						Phone: req.body.Phone,
+						Fax: req.body.Fax
+					})
+				}
+			})
+		})
+	}
+	else {   //Display errors to user
+		var error_msg = ''
+		errors.forEach(function(error) {
+			error_msg += error.msg + '<br>'
+		})
+		req.flash('error', error_msg)
+
+		/**
+		 * Using req.body.name
+		 * because req.param('name') is deprecated
+		 */
+        res.render('customers/edit', {
+            title: 'Edit Customer',
+						CustomerID: req.body.CustomerID,
+            CompanyName: req.body.CompanyName,
+            ContactName: req.body.ContactName,
+						ContactTitle: req.body.ContactTitle,
+            Address: req.body.Address,
+            City: req.body.City,
+						Region: req.body.Region,
+            PostalCode: req.body.PostalCode,
+            Country: req.body.Country,
+						Phone: req.body.Phone,
+						Fax: req.body.Fax
+        })
+    }
+})
+
+// DELETE USER
+app.delete('/delete/(:CustomerID)', function(req, res, next) {
+	var user = { CustomerID: req.params.CustomerID }
+
+	req.getConnection(function(error, conn) {
+		conn.query("DELETE FROM Customers WHERE CustomerID = '"+req.params.CustomerID+"'", user, function(err, result) {
+			if (err) {
+				req.flash('error', err)
+				res.redirect('/customers')
+			} else {
+				req.flash('success', 'Customer deleted successfully! CustomerID = ' + req.params.CustomerID)
+				res.redirect('/customers')
+			}
+		})
+	})
+})
+
+module.exports = app
---
+++ routes/products.js
@@ -0,0 +1,303 @@
+var express = require('express')
+var app = express()
+var path = require('path');
+
+// SHOW LIST OF USERS
+app.get('/list', function(req, res, next) {
+	req.getConnection(function(error, conn) {
+		conn.query('SELECT ProductID,ProductName,SupplierID,CategoryID,QuantityPerUnit,UnitPrice,UnitsInStock,UnitsOnOrder,ReorderLevel,ORD(`Discontinued`) as Discontinued FROM Products ORDER BY ProductID DESC',function(err, rows, fields) {
+			//if(err) throw err
+			if (err) {
+				req.flash('error', err)
+				res.render('products/list', {
+					title: 'Products List',
+					data: ''
+				})
+			} else {
+				// render to views/user/list.ejs template file
+				res.render('products/list', {
+					title: 'Product List',
+					data: rows
+				})
+			}
+		})
+	})
+})
+
+app.get('/', function(req, res, next) {
+				res.redirect('list')
+			}
+		)
+
+// SHOW ADD USER FORM
+app.get('/add', function(req, res, next){
+	// render to views/user/add.ejs
+	req.getConnection(function(error, conn) {
+		conn.query('SELECT ProductID FROM Products  order by ProductID desc LIMIT 1',function(err, rows, fields) {
+			var num = (JSON.parse(rows[0].ProductID));
+			if (err) {
+				req.flash('error', err)
+				res.render('products/add', {
+					title: 'Products List',
+					data: ''
+				})
+			} else {
+			res.render('products/add', {
+				title: 'Add New Product',
+				ProductID: num+1,
+				ProductName: '',
+				SupplierID: '',
+				CategoryID: '',
+				QuantityPerUnit: '',
+				UnitPrice:'',
+				UnitsInStock: '',
+				UnitsOnOrder: '',
+				ReorderLevel: '',
+				Discontinued: ''
+				})
+			}
+		})
+	})
+})
+
+// ADD NEW USER POST ACTION
+app.post('/add', function(req, res, next){
+	req.assert('ProductID', 'ProductID is required').notEmpty()           //Validate name
+	req.assert('ProductName', 'ProductName is required').notEmpty()             //Validate age
+    var errors = req.validationErrors()
+
+    if( !errors ) {   //No errors were found.  Passed Validation!
+
+		var products = {
+			ProductID: req.sanitize('ProductID').escape().trim(),
+			ProductName: req.sanitize('ProductName').escape().trim(),
+			SupplierID: req.sanitize('SupplierID').escape().trim(),
+			CategoryID: req.sanitize('CategoryID').escape().trim(),
+			QuantityPerUnit: req.sanitize('QuantityPerUnit').escape().trim(),
+			UnitPrice: req.sanitize('UnitPrice').escape().trim(),
+			UnitsInStock: req.sanitize('UnitsInStock').escape().trim(),
+			UnitsOnOrder: req.sanitize('UnitsOnOrder').escape().trim(),
+			ReorderLevel: req.sanitize('ReorderLevel').escape().trim(),
+			Discontinued: req.sanitize('Discontinued').escape().trim()
+		}
+
+		req.getConnection(function(error, conn) {
+			conn.query("INSERT INTO Products values ('"+req.body.ProductID+"','"+req.body.ProductName+"','"+req.body.SupplierID+"','"+req.body.CategoryID+"','"+req.body.QuantityPerUnit+"','"+req.body.UnitPrice+"','"+req.body.UnitsInStock+"','"+req.body.UnitsOnOrder+"','"+req.body.ReorderLevel+"',"+Boolean(req.body.Discontinued)+")", function(err, result) {
+				//if(err) throw err
+				if (err) {
+					req.flash('error', err)
+
+					// render to views/user/add.ejs
+					res.render('products/add', {
+						title: 'Add New Product',
+						ProductID: products.ProductID,
+						ProductName: products.ProductName,
+						SupplierID: products.SupplierID,
+						CategoryID: products.CategoryID,
+						QuantityPerUnit: products.QuantityPerUnit,
+						UnitPrice: products.UnitPrice,
+						UnitsInStock: products.UnitsInStock,
+						UnitsOnOrder: products.UnitsOnOrder,
+						ReorderLevel: products.ReorderLevel,
+						Discontinued: products.Discontinued
+					})
+				} else {
+					req.flash('success', 'Data added successfully!')
+
+					// render to views/user/add.ejs
+					res.render('products/add', {
+						title: 'Add New Product',
+						ProductID: '',
+						ProductName: '',
+						SupplierID: '',
+						CategoryID: '',
+						QuantityPerUnit: '',
+						UnitPrice:'',
+						UnitsInStock: '',
+						UnitsOnOrder: '',
+						ReorderLevel: '',
+						Discontinued: ''
+					})
+				}
+			})
+		})
+	}
+	else {   //Display errors to user
+		var error_msg = ''
+		errors.forEach(function(error) {
+			error_msg += error.msg + '<br>'
+		})
+		req.flash('error', error_msg)
+
+		/**
+		 * Using req.body.name
+		 * because req.param('name') is deprecated
+		 */
+        res.render('products/add', {
+            title: 'Add New Product',
+            ProductID: req.body.ProductID,
+            ProductName: req.body.ProductName,
+            SupplierID: req.body.SupplierID,
+						CategoryID: req.body.CategoryID,
+            QuantityPerUnit: req.body.QuantityPerUnit,
+            UnitPrice: req.body.UnitPrice,
+						UnitsInStock: req.body.UnitsInStock,
+            UnitsOnOrder: req.body.UnitsOnOrder,
+            ReorderLevel: req.body.ReorderLevel,
+						Discontinued: req.body.Discontinued
+        })
+    }
+})
+
+// SHOW EDIT USER FORM
+app.get('/edit/(:ProductID)', function(req, res, next){
+	req.getConnection(function(error, conn) {
+		conn.query('SELECT * FROM Products WHERE ProductID = ?', [req.params.ProductID], function(err, rows, fields) {
+			if(err) throw err
+
+			// if user not found
+			if (rows.length <= 0) {
+				req.flash('error', 'Product not found with ProductID = ' + req.params.ProductID)
+				res.redirect('/products')
+			}
+			else { // if user found
+				// render to views/user/edit.ejs template file
+				res.render('products/edit', {
+					title: 'Edit Product',
+					//data: rows[0],
+					ProductID: rows[0].ProductID,
+					ProductName: rows[0].ProductName,
+					SupplierID: rows[0].SupplierID,
+					CategoryID: rows[0].CategoryID,
+					QuantityPerUnit: rows[0].QuantityPerUnit,
+					UnitPrice: rows[0].UnitPrice,
+					UnitsInStock: rows[0].UnitsInStock,
+					UnitsOnOrder: rows[0].UnitsOnOrder,
+					ReorderLevel: rows[0].ReorderLevel,
+					Discontinued: rows[0].Discontinued
+				})
+			}
+		})
+	})
+})
+
+// EDIT USER POST ACTION
+app.put('/edit/(:ProductID)', function(req, res, next) {
+	req.assert('ProductID', 'ProductID is required').notEmpty()
+	req.assert('ProductName', 'ProductName is required').notEmpty()
+
+    var errors = req.validationErrors()
+
+    if( !errors ) {   //No errors were found.  Passed Validation!
+
+		/********************************************
+		 * Express-validator module
+
+		req.body.comment = 'a <span>comment</span>';
+		req.body.username = '   a user    ';
+
+		req.sanitize('comment').escape(); // returns 'a &lt;span&gt;comment&lt;/span&gt;'
+		req.sanitize('username').trim(); // returns 'a user'
+		********************************************/
+		var products = {
+			ProductID: req.sanitize('ProductID').escape().trim(),
+			ProductName: req.sanitize('ProductName').escape().trim(),
+			SupplierID: req.sanitize('SupplierID').escape().trim(),
+			CategoryID: req.sanitize('CategoryID').escape().trim(),
+			QuantityPerUnit: req.sanitize('QuantityPerUnit').escape().trim(),
+			UnitPrice: req.sanitize('UnitPrice').escape().trim(),
+			UnitsInStock: req.sanitize('UnitsInStock').escape().trim(),
+			UnitsOnOrder: req.sanitize('UnitsOnOrder').escape().trim(),
+			ReorderLevel: req.sanitize('ReorderLevel').escape().trim(),
+			Discontinued: req.sanitize('Discontinued').escape().trim()
+		}
+
+		req.getConnection(function(error, conn) {
+			conn.query("UPDATE Products SET ProductID="+req.body.ProductID+",ProductName='"+req.body.ProductName+"',SupplierID="+req.body.SupplierID+",CategoryID="+req.body.CategoryID+",QuantityPerUnit="+req.body.QuantityPerUnit+",UnitPrice="+req.body.UnitPrice+",UnitsInStock="+req.body.UnitsInStock+",UnitsOnOrder="+req.body.UnitsOnOrder+",ReorderLevel="+req.body.ReorderLevel+",Discontinued="+Boolean(req.body.Discontinued)+" WHERE ProductID = " + req.params.ProductID, function(err, result) {
+				//if(err) throw err
+				if (err) {
+					req.flash('error', err)
+
+					// render to views/user/add.ejs
+					res.render('products/edit', {
+						title: 'Edit Products',
+						ProductID: req.params.ProductID,
+						ProductName: req.body.ProductName,
+						SupplierID: req.body.SupplierID,
+						CategoryID: req.body.CategoryID,
+						QuantityPerUnit: req.body.QuantityPerUnit,
+						UnitPrice: req.body.UnitPrice,
+						UnitsInStock: req.body.UnitsInStock,
+						UnitsOnOrder: req.body.UnitsOnOrder,
+						ReorderLevel: req.body.ReorderLevel,
+						Discontinued: req.body.Discontinued
+					})
+				} else {
+					req.flash('success', 'Data updated successfully!')
+
+					// render to views/user/add.ejs
+					res.render('products/edit', {
+						title: 'Edit Product',
+						ProductID: req.params.ProductID,
+						ProductName: req.body.ProductName,
+						SupplierID: req.body.SupplierID,
+						CategoryID: req.body.CategoryID,
+						QuantityPerUnit: req.body.QuantityPerUnit,
+						UnitPrice: req.body.UnitPrice,
+						UnitsInStock: req.body.UnitsInStock,
+						UnitsOnOrder: req.body.UnitsOnOrder,
+						ReorderLevel: req.body.ReorderLevel,
+						Discontinued: req.body.Discontinued
+					})
+				}
+			})
+		})
+	}
+	else {   //Display errors to user
+		var error_msg = ''
+		errors.forEach(function(error) {
+			error_msg += error.msg + '<br>'
+		})
+		req.flash('error', error_msg)
+
+		/**
+		 * Using req.body.name
+		 * because req.param('name') is deprecated
+		 */
+        res.render('products/edit', {
+            title: 'Edit Product',
+						ProductID: req.params.ProductID,
+						ProductName: req.body.ProductName,
+						SupplierID: req.body.SupplierID,
+						CategoryID: req.body.CategoryID,
+						QuantityPerUnit: req.body.QuantityPerUnit,
+						UnitPrice: req.body.UnitPrice,
+						UnitsInStock: req.body.UnitsInStock,
+						UnitsOnOrder: req.body.UnitsOnOrder,
+						ReorderLevel: req.body.ReorderLevel,
+						Discontinued: req.body.Discontinued
+        })
+    }
+})
+
+// DELETE USER
+app.delete('/delete/(:ProductID)', function(req, res, next) {
+	var user = { ProductID: req.params.ProductID }
+
+	req.getConnection(function(error, conn) {
+		conn.query('DELETE FROM Products WHERE ProductID = ' + req.params.ProductID, user, function(err, result) {
+			//if(err) throw err
+			if (err) {
+				req.flash('error', err)
+				// redirect to users list page
+				res.redirect('/products')
+			} else {
+				req.flash('success', 'Product deleted successfully! ProductID = ' + req.params.ProductID)
+				// redirect to users list page
+				res.redirect('/products')
+			}
+		})
+	})
+})
+
+module.exports = app
